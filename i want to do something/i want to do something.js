851d2d51211514e1543d475c37846277353a18cf
BestSpark687090 <smithrya000@pngisd.org>
2022-03-06 14:45:09 -0600

    added a replica of the date thing

---
+++ i want to do something/i want to do something.js
@@ -0,0 +1,21 @@
+console.log(Date());
+e()
+function e(){
+    const day = {
+        dayOfTheWeek: Date().slice(0,3),
+        month: Date().slice(4,7),
+        day: Date().slice(8,10),
+        year: Date().slice(11,15),
+        hour: Date().slice(16,18),
+        minute: Date().slice(19,21),
+        second: Date().slice(22,24),
+        gmt: Date().slice(25,34),
+        timezone: Date().slice(34,57),
+        space: " ",
+        colon: ":",
+    }
+    const partOne = (day.dayOfTheWeek + day.space + day.month + day.space + day.day + day.space);
+    const partTwo = (partOne + day.year + day.space + day.hour + day.colon + day.minute);
+    const partThree = (partTwo + day.colon + day.second + day.space + day.gmt + day.timezone);
+    console.log(partThree);
+}

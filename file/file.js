ef6a0a6bfc54d99c8cc48f317fa8ba0088077a04
BestSpark687090 <bestspark687090@gmail.com>
2022-08-15 11:19:10 -0500

    Set file.js back to normal.

--- file/file.js
+++ file/file.js
@@ -1,14 +1,11 @@
 try {
 var http = require("http")
 var fs = require("fs")
-var file = "E:/RobloxStudioBeta.txt" // original: C:/Users/Ryan Smith/Desktop/Input In Javascript.txt
+var file = "C:/Users/Ryan Smith/Desktop/Input In Javascript.txt"
 var extensionSlice = file.split(".")
-var extension = "text/txt"; // original: "text/"+extensionSlice[1];
+var extension = "text/"+extensionSlice[1];  
     http.createServer(function (req,res){
         fs.readFile(file,function(err,data){	
-	var stringData = data.toString()
-	var dataList = stringData.split("<html>")  
-	console.log(dataList[1])      //original: delete these 3 including this 1 and 2 above
     res.writeHead(200, {"Content-Type":extension});
     if(err == null){
         console.log("Connected with no errors!")

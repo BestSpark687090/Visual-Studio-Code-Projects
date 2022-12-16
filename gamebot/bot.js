1dd34c54602443804fc903301cafb3693e7c310a
BestSpark687090 <smithrya000@pngisd.org>
2022-12-16 10:10:47 -0600

    ok cool i guess thanks discord.js

---
+++ gamebot/bot.js
@@ -0,0 +1,16 @@
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+var a = require('discord.js')
+const dotenv = require('dotenv')
+dotenv.config()
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(process.env.token);
